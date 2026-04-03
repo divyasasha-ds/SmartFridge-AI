c52746e9fa3095a3c44f1304ad4465a1c2a79e98
DIVYA SASHA <divyasasha.ds@gmail.com>
2026-04-03 10:35:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
+from fastapi import FastAPI
+from env import SmartFridgeEnv
 
+app = FastAPI()
+
+env = SmartFridgeEnv()
+
+@app.get("/")
+def home():
+    return {"message": "Smart Fridge Environment Running"}
+
+@app.get("/reset")
+def reset():
+    state = env.reset()
+    return {"state": state}
+
+@app.get("/state")
+def get_state():
+    return {"state": env.get_state()}
+
+@app.post("/step")
+def step(action: str):
+    state, reward, done = env.step(action)
+    return {
+        "state": state,
+        "reward": reward,
+        "done": done
+    }
